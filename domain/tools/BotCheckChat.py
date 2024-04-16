a7403d0d16f2c086048dee1b6ce6fb178c83346c
artem <artemqq5@gmail.com>
2024-04-16 17:04:39 +0300

    Update BotCheckChat.py

--- domain/tools/BotCheckChat.py
+++ domain/tools/BotCheckChat.py
@@ -19,7 +19,7 @@ async def check_bot_membership(bot, chat_id):
                 else:
                     results += f"{chat['group_id']} | {chat['title']}\n{chat['link']}\n\nНе вийшло оновити лінку на групу (скоріше за все бот не доданий як адмін)\n\nНе виправлено ❌"
         except TelegramForbiddenError as e:
-            if e.message.__contains__("bot was kicked from the group chat"):
+            if e.message.__contains__("bot was kicked from the group chat") or e.message.__contains__("bot was kicked from the supergroup chat"):
                 results += f"{chat['group_id']} | {chat['title']}\n{chat['link']}\n\nБот був кікнутий з групи (Групу видалено з бази)\n\nВиправлено ✅"
             else:
                 results += f"{chat['group_id']} | {chat['title']}\n{chat['link']}\n\n{e.message}\n\n Не виправлено ❌"
